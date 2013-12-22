370d45e9e9d9cab37aeef998d8ce8dc1da8ff101
noj33 <jonfriese33@gmail.com>
2013-12-22 02:04:04 -0800

    start of todos

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(todos) {
+  return function(req, res){
+    res.render('index', {
+      title: 'Express',
+      todos : todos
+    });
+  };
+};
+
+exports.addTodo = function(todos) {
+  return function(req, res) {
+    todos.push(req.body);
+    res.json({ todos : todos });
+  };
 };
