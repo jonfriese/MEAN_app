697cc9863f8ad2b90519c40e473ba685b7310112
noj33 <jonfriese33@gmail.com>
2013-12-22 02:27:44 -0800

    working todo app

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 var Mongoose = require('mongoose');
 var db = Mongoose.createConnection('localhost', 'mytestapp');
 
+var TodoSchema = require('./models/Todo.js').TodoSchema;
+var Todo = db.model('todos', TodoSchema);
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -30,25 +33,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var todos = [
-  { description : "Buy eggs",
-    due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 1 day from now
-    done : false
-  },
-  { description : "Write next blog post",
-    due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
-    done : false
-  },
-  { description : "Build todo list app",
-    due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
-    done : true
-  },
-];
-
-app.get('/', routes.index(todos));
+app.get('/', routes.index(Todo));
 app.get('/users', user.list);
+app.get('/todos.json', routes.get(Todo));
 
-app.post('/todo.json', routes.addTodo(todos));
+app.put('/todo/:id.json', routes.update(Todo));
+
+app.post('/todo.json', routes.addTodo(Todo));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/javascripts/controllers/TodoListController.js
+++ public/javascripts/controllers/TodoListController.js
@@ -1,4 +1,4 @@
-function TodoListController($scope, $http) {
+function TodoListController($scope, $http, $timeout) {
   $scope.todos = [];
   $scope.newTodo = {
     done : false,
@@ -13,10 +13,38 @@ function TodoListController($scope, $http) {
     $scope.todos = todos;
   };
 
+  $scope.update = function(todo) {
+    $http.put('/todo/' + todo._id + '.json', todo).success(function(data) {
+      if (!data.todo) {
+        alert(JSON.stringify(data));
+      }
+    });
+  };
+
+  $scope.updateList = function() {
+    $http.get('/todos.json').success(function(data) {
+      $scope.todos = data.todos;
+    });
+
+    $timeout(function() {
+      $scope.updateList();
+    }, 30 * 60 * 1000); // update every 30 minutes;
+  };
+
+  $timeout(function() {
+    $scope.updateList();
+  }, 30 * 60 * 1000); // update every 30 minutes;
+
+  $scope.updateList();
+
   $scope.addNewTodo = function() {
     $http.post('/todo.json', $scope.newTodo).success(function(data) {
-      $scope.todos = data.todos;
-      $scope.newTodo.description = '';
+      if (data.todo) {
+        $scope.todos.push(data.todo);
+        $scope.newTodo.description = '';
+      } else {
+        alert(JSON.stringify(data));
+      }
     });
   };
 }
